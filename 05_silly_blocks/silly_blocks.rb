4ed79ca7edc6dc54c42a708cc1ab8754abda20ac
Mateusz <kwiatkiwoknie@gmail.com>
2015-08-05 09:46:32 +0200

    Minor fix

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -2,10 +2,10 @@ def reverser
   (yield.split(/ /).each { |x| x.reverse!}).join(' ')
 end
 
-def adder (x = 1)
+def adder (x=1)
   yield + x
 end
 
-def repeater (x = 1)
+def repeater (x=1)
   x.times { yield }
 end
