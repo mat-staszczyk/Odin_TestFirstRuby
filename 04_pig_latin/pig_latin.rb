5cb4ef95d44df57bf6625bc6f0d64fc66f231aaf
Mateusz Staszczyk <mat.staszczyk@gmail.com>
2015-09-09 07:25:30 +0200

    Update pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,39 +1,15 @@
-def translate (subject)
-  text = subject.split
-  new_arr = []
-
-  for word in text
-    punct = nil
-    if /[[:punct:]]$/.match(word)
-      punct = word[-1, 1]
-      word.chop!
-    end
-    if /^[[:upper:]]/.match(word[0])
-      capitalize_me = true
-      word.downcase!
-    end
-    if word[/^[aeiou]/]
-      word = word + "ay"
+def translate (words)
+  words = words.split
+  words.map do |word|
+    if /^[aeiouy]/ === word then
+      word << "ay"
+    elsif /^(squ|sch|thr)/ === word then
+      word << "#{word.slice!(0,3)}ay"
+    elsif /^(qu|ch|br|th)/ === word then
+      word << "#{word.slice!(0,2)}ay" 
     else
-      if word[/^qu/]
-        rest = word.slice!(/^qu/) + "ay"
-      elsif word[/.qu/]
-        rest = word.slice!(/^.qu/) +"ay"
-      else
-        rest = word.slice!(0...(word.index(/[aeiou]/,1))) + "ay"
-      end
-      word = word << rest
+      word << "#{word.slice!(0)}ay"
     end
-    if capitalize_me
-      word.capitalize!
-      capitalize_me = nil     
-    end
-    if punct
-      word << punct 
-      punct = nil
-    end 
-    new_arr << word
   end
-
-  new_arr.join(" ")
+  words.join(" ")
 end
