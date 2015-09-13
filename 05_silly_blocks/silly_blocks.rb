00614cbe4b9ac0741ff6d198ba6a1c4bda75c303
Mateusz <kwiatkiwoknie@gmail.com>
2015-09-13 16:57:49 +0200

    Reverser method improved

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,11 +1,3 @@
 def reverser 
-  (yield.split(/ /).each { |x| x.reverse!}).join(' ')
-end
-
-def adder (x=1)
-  yield + x
-end
-
-def repeater (x=1)
-  x.times { yield }
+  yield.split(' ').map(&:reverse).join(' ')
 end
