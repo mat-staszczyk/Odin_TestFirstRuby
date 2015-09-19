e94dbc4d800c43376d0cc790b2201b2ced9341aa
Mateusz <kwiatkiwoknie@gmail.com>
2015-09-19 15:07:26 +0200

    Temperature partial solution added

---
+++ 10_temperature_object/temperature.rb
@@ -0,0 +1,29 @@
+class Temperature
+
+  def ftoc (f)
+    (f - 32) * (5.0/9.0)
+  end
+
+  def ctof (c)
+    c * 1.8 + 32
+  end
+
+  def initialize (hash)
+    if hash.key?(:f) then
+      @fahrenheit = hash[:f]
+      @celsius = ftoc (@fahrenheit)
+    elsif hash.key?(:c)
+      @celsius = hash[:c]
+      @fahrenheit = ctof (@celsius)
+    end
+  end
+
+  def in_fahrenheit
+    @fahrenheit
+  end
+
+  def in_celsius
+    @celsius
+  end
+
+end
