237c281524653ba9bfc893480fbbdab9e2b36447
Mateusz <kwiatkiwoknie@gmail.com>
2015-09-24 08:46:24 +0200

    Solution update

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -3,5 +3,13 @@ class Dictionary
     @entries ||= {}
   end
   
+  def add entry
+    entries[entry] = nil if entry.is_a? String
+    entries.merge!(entry) if entry.is_a? Hash
+  end
+  
+  def keywords
+    entries.keys.map(&:to_s).sort
+  end
 
 end
