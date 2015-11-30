e1126d49595a8880f194c3f824d216b5bafe5f09
sleaz0id <mat.staszczyk@gmail.com>
2015-11-30 14:01:26 +0100

    helper array added

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -9,6 +9,7 @@ module Inwords
   def teens
     ones = %w[ zero one two three four five
                six seven eight nine ].to_a
+    teens = %w[ ten eleven twelve ].to_a
   end
 
   def more_digits
