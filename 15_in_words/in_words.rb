d620cf22b9888e33c750b8969464f10c07df8927
sleaz0id <mat.staszczyk@gmail.com>
2015-12-02 10:24:47 +0100

    Solution update

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -14,6 +14,20 @@ module Inwords
     						 eigh nine ].to_a
 
     num = self.to_s
+    
+    if (0..9).include? self then
+      ones[self]
+    else
+      if num[0] == "1" then
+        u_index = num[1].to_i
+        ((0..2).include? u_index) ? "#{teens[u_index]}" : "#{prefix[u_index]}teen"
+      else
+        d_index = num[0].to_i
+        s = (d_index == 4) ? "forty" : "#{prefix[d_index]}ty"
+        s << " #{ones[num[1].to_i]}" if num[1] != "0"
+        s
+      end
+    end
 
   end
 
